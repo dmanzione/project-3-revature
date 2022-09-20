3452120fc0fe5308ef7a959b48919c184a61903b
Warren Lee <wplee327@users.noreply.github.com>
2022-09-20 15:09:52 -0400

    Specify name of products table

--- src/main/java/com/revature/models/Product.java
+++ src/main/java/com/revature/models/Product.java
@@ -8,11 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
 public class Product {
 
     @Id
