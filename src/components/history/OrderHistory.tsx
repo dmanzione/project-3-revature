5ddbd33de80cd9c83c2107533748e4f27ed309c4
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-09-21 09:38:14 -0700

    added router funtionality

--- src/components/history/OrderHistory.tsx
+++ src/components/history/OrderHistory.tsx
@@ -1,10 +1,11 @@
 import {useNavigate} from 'react-router-dom';
 
 function OrderHistory() {
-    return
+    return (
         <div>
             <h1>Order History</h1>
         </div>
+        )
 }
 
 export default OrderHistory;
