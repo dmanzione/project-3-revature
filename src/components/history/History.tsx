90b5190905aac2b97757339a6dfb8eff9566d7a1
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-09-21 09:24:13 -0700

    created history component

---
+++ src/components/history/History.tsx
@@ -0,0 +1,10 @@
+import {useNavigate} from 'react-router-dom';
+
+function OrderHistory() {
+    return
+        <div>
+            <h1>Order History</h1>
+        </div>
+}
+
+export default OrderHistory;
