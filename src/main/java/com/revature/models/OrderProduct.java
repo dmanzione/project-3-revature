51406ffc73fca9721d5a6860bea81b895b8570d2
Tariq Saddler <Tariq658@revature.net>
2022-09-22 16:06:05 -0400

    still working on order history

--- src/main/java/com/revature/models/OrderProduct.java
+++ src/main/java/com/revature/models/OrderProduct.java
@@ -9,6 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,6 +30,7 @@ public class OrderProduct {
 	private Order order;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id")
+	@JsonBackReference
 	private Product product;	
 	private int quantity;
 	private double subtotal;
