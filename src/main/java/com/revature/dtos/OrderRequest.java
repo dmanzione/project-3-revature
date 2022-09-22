cd5bbc9a5f9128ee591667985d74b91418ee3f3c
Tariq Saddler <Tariq658@revature.net>
2022-09-22 11:50:45 -0400

    Created order post request and removed CORS in controller classes

---
+++ src/main/java/com/revature/dtos/OrderRequest.java
@@ -0,0 +1,20 @@
+package com.revature.dtos;
+
+import java.util.List;
+
+import com.revature.models.Order;
+import com.revature.models.OrderProduct;
+import com.revature.models.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+	Order order;
+	List<OrderProduct> orderProducts;
+	
+}
