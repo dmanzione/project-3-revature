a7f17d05ad074a55daa63f44c71cf69bdf396368
Tariq Saddler <Tariq658@revature.net>
2022-09-22 15:41:01 -0400

    Working on orders

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -2,9 +2,16 @@ package com.revature.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Data
 @Entity
@@ -20,4 +27,10 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonManagedReference
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<Order> orders;
+
 }
